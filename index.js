cb86a74ecec4fad187f0460540de0cc3ba5f2953
omarmitma <75546685+omarmitma@users.noreply.github.com>
2022-01-21 21:33:07 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -5,8 +5,38 @@ let dataLocation=document.getElementById('dataLocation');
 let dataTime=document.getElementById('dataTime');
 let dataISP=document.getElementById('dataISP');
 
-let lat=34.04915 ;
+let lat = 34.04915 ;
 let lng =-118.09462;
+
+/* Obtener la ip del cliente */
+async function getIpClient(update) {
+  try {
+    const response = await axios.get('https://api.ipify.org?format=json');
+    console.log(response);
+    viewMapByIp(response.data.ip, update)
+  } catch (error) {
+    console.error(error);
+  }
+}
+getIpClient(false);
+
+
+function getLocation(){
+  /* Obtener coordenadas del cliente */
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      lat = position.coords.latitude;
+      lng = position.coords.longitude;
+      updatemap(lat,lng)
+    },err => {
+      getIpClient(true);
+    });
+  }
+}
+getLocation();
+
+
+/* Ver el mapa */
 function viewMap(){
     var coord = {lat:lat ,lng: lng};
     var map = new google.maps.Map(document.getElementById('map'),{
@@ -29,31 +59,44 @@ function viewMap(){
     });
 }
 btnSearchIp.addEventListener('click',()=>{
-  let inputIp=document.getElementById('inputIp');
-  var isValid = isValidIP(inputIp.value);
+  viewMapByIp(inputIp.value,true)
+});
+
+/* Funcion para ver el mapa por ip que pasemos */
+function viewMapByIp(ip, update){
+  var isValid = isValidIP(ip);
   if(isValid){
     const xhttp=new XMLHttpRequest();
-    xhttp.open('GET',`https://geo.ipify.org/api/v1?apiKey=at_7GtudJlRcXYBe46vflmvred5Cfcqg&ipAddress=${inputIp.value}`, true);
+    xhttp.open('GET',`https://geo.ipify.org/api/v1?apiKey=at_7GtudJlRcXYBe46vflmvred5Cfcqg&ipAddress=${ip}`, true);
     xhttp.send();
     xhttp.onreadystatechange=function(){
       if (this.readyState==4 && this.status==200) {
         let datos=JSON.parse(this.responseText);
 
-        /* Ver en el mapa */
-        updatemap(datos.location.lat,datos.location.lng);
-        /* Pintar los datos */
-        dataIp.textContent=datos.ip;
-        dataLocation.textContent=datos.location.city+", "+datos.location.region;
-        console.log(datos);
-        dataTime.textContent=datos.location.timezone;
-        dataISP.textContent=datos.isp;
+        /* Actualizar el mapa */
+        if (update){
+          updatemap(datos.location.lat,datos.location.lng);
+        }
+        
+
+        viewData(datos);
       } 
     };
   }else{
     alert('Direccion ip invalida')
   }
-  
-});
+}
+
+
+/* Pintar los datos */
+function viewData(datos){
+  dataIp.textContent=datos.ip;
+  dataLocation.textContent=datos.location.city+", "+datos.location.region;
+  console.log(datos);
+  dataTime.textContent=datos.location.timezone;
+  dataISP.textContent=datos.isp;
+}
+
 function updatemap(lt,lg){
   lat=lt;
   lng =lg;
